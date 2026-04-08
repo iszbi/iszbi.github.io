3b0922ab92b6b4987ae4c8bc1ac946c38490b040
iszbi <izml@outlook.com.au>
2026-04-08 17:54:56 +1000

    some changes to splash before i push to github

--- script/splash/splash.js
+++ script/splash/splash.js
@@ -43,6 +43,8 @@ function measureSplash(text, font, size) {
 const dosfont = new FontFace("Perfect DOS VGA\ 437 Win", "url('https://unpkg.com/xp.css/dist/PerfectDOSVGA437Win.woff2') format('woff2')");
 dosfont.weight = 400;
 
+var timeout_id = -1;
+
 initSplash().then(() => {    
     dosfont.load().then(
         () => {
@@ -52,14 +54,17 @@ initSplash().then(() => {
             
             console.log(`text width: ${splash_width}`);
             console.log(`div width: ${div_width}`);
-            if(splash_width > div_width) {
-                setTimeout(() => splash.classList.add("horizontal-scrolling"), 2500);
+            if(timeout_id == -1) {
+                if(splash_width > div_width) {
+                    timeout_id = setTimeout(() => splash.classList.add("horizontal-scrolling"), 2500);
+                }
             }
             
         }, (err) => alert(err));
 });
     
 document.defaultView.addEventListener("resize", (event) => {
+    console.log("resize triggered");
     var splash_width = measureSplash(current, dosfont, 16);
     var div_width = document.getElementById("splash-container").getBoundingClientRect().width;
 
@@ -67,14 +72,22 @@ document.defaultView.addEventListener("resize", (event) => {
     console.log(`div width: ${div_width}`);
 
     if(splash_width > div_width) {
-        setTimeout(() => {
-            if(!splash.classList.contains("horizontal-scrolling"))
-                splash.classList.add("horizontal-scrolling");
-        }, 2500);
+        if(timeout_id == -1) {
+            timeout_id = setTimeout(() => {
+                if(!splash.classList.contains("horizontal-scrolling"))
+                    splash.classList.add("horizontal-scrolling");
+            }, 2500);
+        }
+        
     } else {
         console.log("yay i'm here");
         if(splash.classList.contains("horizontal-scrolling")) {
             splash.classList.remove("horizontal-scrolling");
+        }
+
+        if(timeout_id != -1) {
+            clearTimeout(timeout_id);
+            timeout_id = -1;
         }
     }
 });
